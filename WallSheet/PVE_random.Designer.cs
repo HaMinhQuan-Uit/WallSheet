94e0aff0cf1b7ffc8bcbc22c3f8a208baa692521
Teinsky <ndcthanh208@gmail.com>
2024-06-02 00:00:55 +0700

    add PvE

---
+++ WallSheet/PVE_random.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WallSheet
+{
+    partial class PVE_random
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Price = new System.Windows.Forms.TextBox();
+            this.Quantity = new System.Windows.Forms.TextBox();
+            this.Budget = new System.Windows.Forms.TextBox();
+            this.Target = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Turn = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Price
+            // 
+            this.Price.Location = new System.Drawing.Point(93, 135);
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            this.Price.Size = new System.Drawing.Size(100, 20);
+            this.Price.TabIndex = 0;
+            this.Price.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Quantity
+            // 
+            this.Quantity.Location = new System.Drawing.Point(202, 135);
+            this.Quantity.Name = "Quantity";
+            this.Quantity.ReadOnly = true;
+            this.Quantity.Size = new System.Drawing.Size(100, 20);
+            this.Quantity.TabIndex = 1;
+            this.Quantity.TextChanged += new System.EventHandler(this.Quantity_TextChanged);
+            // 
+            // Budget
+            // 
+            this.Budget.Location = new System.Drawing.Point(93, 174);
+            this.Budget.Name = "Budget";
+            this.Budget.ReadOnly = true;
+            this.Budget.Size = new System.Drawing.Size(100, 20);
+            this.Budget.TabIndex = 2;
+            // 
+            // Target
+            // 
+            this.Target.Location = new System.Drawing.Point(202, 174);
+            this.Target.Name = "Target";
+            this.Target.ReadOnly = true;
+            this.Target.Size = new System.Drawing.Size(100, 20);
+            this.Target.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Quantity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Budget";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(199, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Target";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Sell";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(93, 299);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 24);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Buy";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Turn
+            // 
+            this.Turn.Location = new System.Drawing.Point(93, 56);
+            this.Turn.Name = "Turn";
+            this.Turn.ReadOnly = true;
+            this.Turn.Size = new System.Drawing.Size(100, 20);
+            this.Turn.TabIndex = 11;
+            this.Turn.TextChanged += new System.EventHandler(this.Turn_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(93, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Turn";
+            // 
+            // PVE_random
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Turn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Target);
+            this.Controls.Add(this.Budget);
+            this.Controls.Add(this.Quantity);
+            this.Controls.Add(this.Price);
+            this.Name = "PVE_random";
+            this.Text = "PVE_random";
+            this.Load += new System.EventHandler(this.PVE_random_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Price;
+        private System.Windows.Forms.TextBox Quantity;
+        private System.Windows.Forms.TextBox Budget;
+        private System.Windows.Forms.TextBox Target;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Turn;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ WallSheet/PVE_random.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WallSheet
+{
+    public partial class PVE_random : Form
+    {
+        public PVE_random()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PVE_random_Load(object sender, EventArgs e)
+        {
+
+            
+                Random random = new Random();
+
+                // Generate random values
+                int turn = random.Next(10, 31);
+                decimal price = (decimal)(random.NextDouble() * (99999.99 - 0.09) + 0.09);
+                decimal budget = (decimal)(random.NextDouble() * (1000000 - 0.1) + 0.1);
+                decimal target = (decimal)(random.NextDouble() * (10000000 - 1000) + 1000);
+                int quantity = random.Next(1, 10001);
+
+            // Ensure target is always more than budget
+            while (target <= budget)
+                {
+                    target = (decimal)(random.NextDouble() * (10000000 - 1000) + 1000);
+                }
+
+                // Set the values to the textboxes
+                Turn.Text = turn.ToString();
+                Price.Text = price.ToString("F2"); // F2 for 2 decimal places
+                Budget.Text = budget.ToString("F2"); // F2 for 2 decimal places
+                Target.Text = target.ToString("F2"); // F2 for 2 decimal places
+                Quantity.Text = quantity.ToString(); // Set Quantity
+
+
+        }
+
+        private void Quantity_TextChanged(object sender, EventArgs e)
+        {
+        }
+        
+
+        private void Turn_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
