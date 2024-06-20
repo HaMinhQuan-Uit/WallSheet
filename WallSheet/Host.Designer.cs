7a908331a978f7645b47cc380a7bdf711521661e
HaMinhQuan_Uit <haminhquan091203@gmail.com>
2024-06-20 10:21:06 +0700

    fix UI

--- WallSheet/Host.Designer.cs
+++ WallSheet/Host.Designer.cs
@@ -116,6 +116,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(521, 450);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.Show);
