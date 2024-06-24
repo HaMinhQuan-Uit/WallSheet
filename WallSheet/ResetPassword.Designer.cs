96781b0937de030dd157275b3944e8d122b0ab67
tam0624 <letamdinh46@gmail.com>
2024-06-24 11:34:14 +0700

    update resetpassword.cs

--- WallSheet/ResetPassword.Designer.cs
+++ WallSheet/ResetPassword.Designer.cs
@@ -1,68 +1,104 @@
-﻿namespace WallSheet
+﻿// ResetPassword.Designer.cs
+namespace WallSheet
 {
-    // ResetPassword.Designer.cs (mã này sẽ được sinh tự động nếu bạn thêm điều khiển qua Designer)
     partial class ResetPassword
     {
-        private System.Windows.Forms.TextBox textBoxNewPassword;
-        private System.Windows.Forms.Button buttonUpdatePassword;
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblNewPassword;
+        private System.Windows.Forms.TextBox txbNewPassword;
+        private System.Windows.Forms.Label lblConfirmPassword;
+        private System.Windows.Forms.TextBox txbConfirmPassword;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnCancel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
         private void InitializeComponent()
         {
-            this.textBoxNewPassword = new System.Windows.Forms.TextBox();
-            this.buttonUpdatePassword = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblNewPassword = new System.Windows.Forms.Label();
+            this.txbNewPassword = new System.Windows.Forms.TextBox();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.txbConfirmPassword = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBoxNewPassword
+            // lblNewPassword
             // 
-            this.textBoxNewPassword.Location = new System.Drawing.Point(178, 15);
-            this.textBoxNewPassword.Margin = new System.Windows.Forms.Padding(4);
-            this.textBoxNewPassword.Name = "textBoxNewPassword";
-            this.textBoxNewPassword.Size = new System.Drawing.Size(183, 22);
-            this.textBoxNewPassword.TabIndex = 0;
+            this.lblNewPassword.AutoSize = true;
+            this.lblNewPassword.Location = new System.Drawing.Point(12, 15);
+            this.lblNewPassword.Name = "lblNewPassword";
+            this.lblNewPassword.Size = new System.Drawing.Size(89, 16);
+            this.lblNewPassword.TabIndex = 0;
+            this.lblNewPassword.Text = "Mật khẩu mới:";
             // 
-            // buttonUpdatePassword
+            // txbNewPassword
             // 
-            this.buttonUpdatePassword.BackColor = System.Drawing.SystemColors.Info;
-            this.buttonUpdatePassword.Location = new System.Drawing.Point(13, 47);
-            this.buttonUpdatePassword.Margin = new System.Windows.Forms.Padding(4);
-            this.buttonUpdatePassword.Name = "buttonUpdatePassword";
-            this.buttonUpdatePassword.Size = new System.Drawing.Size(350, 28);
-            this.buttonUpdatePassword.TabIndex = 1;
-            this.buttonUpdatePassword.Text = "Update Password";
-            this.buttonUpdatePassword.UseVisualStyleBackColor = false;
-            this.buttonUpdatePassword.Click += new System.EventHandler(this.buttonUpdatePassword_Click);
+            this.txbNewPassword.Location = new System.Drawing.Point(164, 9);
+            this.txbNewPassword.Name = "txbNewPassword";
+            this.txbNewPassword.Size = new System.Drawing.Size(159, 22);
+            this.txbNewPassword.TabIndex = 1;
+            this.txbNewPassword.UseSystemPasswordChar = true;
             // 
-            // label1
+            // lblConfirmPassword
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.Info;
-            this.label1.Location = new System.Drawing.Point(12, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 16);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "NEW PASSWORD";
+            this.lblConfirmPassword.AutoSize = true;
+            this.lblConfirmPassword.Location = new System.Drawing.Point(12, 41);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(122, 16);
+            this.lblConfirmPassword.TabIndex = 2;
+            this.lblConfirmPassword.Text = "Xác nhận mật khẩu:";
+            // 
+            // txbConfirmPassword
+            // 
+            this.txbConfirmPassword.Location = new System.Drawing.Point(164, 38);
+            this.txbConfirmPassword.Name = "txbConfirmPassword";
+            this.txbConfirmPassword.Size = new System.Drawing.Size(159, 22);
+            this.txbConfirmPassword.TabIndex = 3;
+            this.txbConfirmPassword.UseSystemPasswordChar = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(248, 64);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Đặt lại";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(164, 64);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Hủy";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // ResetPassword
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WallSheet.Properties.Resources.cach_choi_ma_soi_1;
-            this.ClientSize = new System.Drawing.Size(379, 103);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.buttonUpdatePassword);
-            this.Controls.Add(this.textBoxNewPassword);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.ClientSize = new System.Drawing.Size(387, 104);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txbConfirmPassword);
+            this.Controls.Add(this.lblConfirmPassword);
+            this.Controls.Add(this.txbNewPassword);
+            this.Controls.Add(this.lblNewPassword);
             this.Name = "ResetPassword";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Reset Password";
-            this.Load += new System.EventHandler(this.ResetPassword_Load);
+            this.Text = "Đặt lại mật khẩu";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
-
-        private System.Windows.Forms.Label label1;
     }
-
 }
--- WallSheet/ResetPassword.cs
+++ WallSheet/ResetPassword.cs
@@ -1,8 +1,13 @@
 ﻿// ResetPassword.cs
+using Firebase_Project;
 using FireSharp.Config;
 using FireSharp.Interfaces;
 using FireSharp.Response;
 using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WallSheet
@@ -10,7 +15,6 @@ namespace WallSheet
     public partial class ResetPassword : Form
     {
         private string email;
-
         IFirebaseConfig ifc = new FirebaseConfig()
         {
             AuthSecret = "UYaSCRpXgQFANuKeFHlhq5l6DZLMcmtNXkZjL1nJ",
@@ -34,48 +38,87 @@ namespace WallSheet
                 {
                     throw new Exception("Client is null, failed to create FireSharp client.");
                 }
-                MessageBox.Show("Firebase connection established successfully");
+                MessageBox.Show("Kết nối Firebase thành công");
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Failed to initialize Firebase client: {ex.Message}", "Cảnh báo!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show($"Lỗi khi khởi tạo client Firebase: {ex.Message}", "Cảnh báo!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
-        private async void buttonUpdatePassword_Click(object sender, EventArgs e)
+        private async void btnReset_Click(object sender, EventArgs e)
         {
-            string newPassword = textBoxNewPassword.Text;
+            string newPassword = txbNewPassword.Text;
+            string confirmPassword = txbConfirmPassword.Text;
 
-            if (!string.IsNullOrEmpty(newPassword))
+            if (string.IsNullOrWhiteSpace(newPassword) || string.IsNullOrWhiteSpace(confirmPassword))
             {
-                try
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin", "Cảnh báo!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (newPassword != confirmPassword)
+            {
+                MessageBox.Show("Mật khẩu không khớp", "Cảnh báo!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                FirebaseResponse response = await client.GetAsync("Users");
+                var users = response.ResultAs<Dictionary<string, MyUser>>();
+                bool passwordUpdated = false;
+
+                foreach (var user in users)
                 {
-                    var data = new { password = newPassword };
-                    FirebaseResponse response = await client.UpdateAsync($"users/{email}/password", data);
-                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    if (user.Value.Email == email)
                     {
-                        MessageBox.Show("Password updated successfully");
-                        this.Close();
-                    }
-                    else
-                    {
-                        MessageBox.Show("Failed to update password");
+                        string hashedNewPassword = ComputeSha256Hash(newPassword);
+                        if (user.Value.Password == hashedNewPassword)
+                        {
+                            MessageBox.Show("Mật khẩu mới phải khác mật khẩu cũ", "Cảnh báo!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+
+                        user.Value.Password = hashedNewPassword;
+                        await client.UpdateAsync($"Users/{user.Key}", user.Value);
+                        passwordUpdated = true;
                     }
                 }
-                catch (Exception ex)
+
+                if (passwordUpdated)
                 {
-                    MessageBox.Show($"Error updating password: {ex.Message}");
+                    MessageBox.Show("Đặt lại mật khẩu thành công!", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy người dùng với email này", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Please enter a new password");
+                MessageBox.Show($"Lỗi khi đặt lại mật khẩu: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        private void ResetPassword_Load(object sender, EventArgs e)
+        private string ComputeSha256Hash(string rawData)
         {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
 
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
